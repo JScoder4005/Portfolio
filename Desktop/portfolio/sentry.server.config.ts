6907f59494b64a4ce9eeaf4c4f996358fb2d971b
Uday kumar M <136188719+JScoder4005@users.noreply.github.com>
2024-10-01 18:14:16 +0530

    Portfolio using nextJs

---
+++ Desktop/portfolio/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e09ebcefb92281d762f419a90a6f329c@o4507656157134848.ingest.us.sentry.io/4507656159363072",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
